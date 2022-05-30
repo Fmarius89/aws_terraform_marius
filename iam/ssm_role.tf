f157b0689f0dd96ef82d18e4cc02685a5e249b0b
fmarius89 <fetti_marius89@yahoo.com>
2022-05-30 10:04:28 +0300

    Actions

--- iam/ssm_role.tf
+++ iam/ssm_role.tf
@@ -27,12 +27,12 @@ resource "aws_iam_role_policy_attachment" "test_attach2" {
 }
 
 
-resource "aws_ssm_activation" "foo" {
+resource "aws_ssm_activation" "test_role2" {
   name               = "test_ssm_activation"
   description        = "Test"
   iam_role           = "${aws_iam_role.test_role2.id}"
   registration_limit = "5"
-  #depends_on         = ["aws_iam_role_policy_attachment.test_attach"]
+  depends_on         = [aws_iam_role_policy_attachment.test_attach]
 }
 
 resource "aws_iam_instance_profile" "test_profile" {
